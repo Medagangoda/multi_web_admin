147526521dd9f54e29d2219e03e05e7483dfbd53
Medagangoda <140582352+Medagangoda@users.noreply.github.com>
2024-12-13 20:11:51 +0530

    Upload Banner UI

--- multi_web_admin/lib/views/screens/side_bar_screens/upload_banner_screen.dart
+++ multi_web_admin/lib/views/screens/side_bar_screens/upload_banner_screen.dart
@@ -1,25 +1,97 @@
-
-
-
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 
-class UploadBannerScreen extends StatelessWidget {
+class UploadBannerScreen extends StatefulWidget {
   static const String routeName = '\UploadBannerScreen';
+
+  @override
+  State<UploadBannerScreen> createState() => _UploadBannerScreenState();
+}
+
+class _UploadBannerScreenState extends State<UploadBannerScreen> {
+  dynamic _image;
+
+  String? fileName;
+
+  pickImage() async {
+    FilePickerResult? result = await FilePicker.platform
+        .pickFiles(allowMultiple: false, type: FileType.image);
+
+    if (result != null) {
+      setState(() {
+        _image = result.files.first.bytes;
+
+        fileName = result.files.first.name;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-        child: Container(
-          alignment: Alignment.topLeft,
-          padding: const EdgeInsets.all(10),
-          child: const Text(
-            'Upload Banners',
-            style: TextStyle(
-              fontWeight: FontWeight.w700,
-              fontSize: 36,
+      child: Column(
+        children: [
+          Container(
+            alignment: Alignment.topLeft,
+            padding: const EdgeInsets.all(10),
+            child: const Text(
+              'Banners',
+              style: TextStyle(
+                fontWeight: FontWeight.w700,
+                fontSize: 36,
+              ),
             ),
           ),
-        ),
-      );
+          Divider(
+            color: Colors.grey,
+          ),
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(14.0),
+                child: Column(
+                  children: [
+                    Container(
+                      height: 140,
+                      width: 140,
+                      decoration: BoxDecoration(
+                        color: Colors.grey.shade400,
+                        border: Border.all(color: Colors.grey.shade800),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: _image != null
+                          ? Image.memory(_image, fit:  BoxFit.cover,)
+                          : Center(
+                              child: Text('Banner'),
+                            ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    ElevatedButton(
+                      // style: ElevatedButton.styleFrom(
+                      //     backgroundColor: Colors.yellow.shade900),
+                      onPressed: () {
+                        pickImage();
+                      },
+                      child: Text('Upload'),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                width: 30,
+              ),
+              ElevatedButton(
+                // style: ElevatedButton.styleFrom(
+                //     backgroundColor: Colors.yellow.shade900),
+                onPressed: () {},
+                child: Text('save'),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
