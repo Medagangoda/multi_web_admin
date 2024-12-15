1bd96b8d7fe8baf57e0a2b60458eeee4e54cf7ba
Medagangoda <140582352+Medagangoda@users.noreply.github.com>
2024-12-15 17:51:10 +0530

    Create Vendor Header

--- multi_web_admin/lib/views/screens/side_bar_screens/vendors_screen.dart
+++ multi_web_admin/lib/views/screens/side_bar_screens/vendors_screen.dart
@@ -1,24 +1,55 @@
-
-
 import 'package:flutter/material.dart';
 
 class VendorsScreen extends StatelessWidget {
   static const String routeName = '\VendorsScreen';
 
+  Widget _rowHeader(String text, int flex) {
+    return Expanded(
+        flex: 2,
+        child: Container(
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.grey.shade700),
+            color: Colors.yellow.shade800,
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              text,
+              style:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+            ),
+          ),
+        ));
+  }
+
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-        child: Container(
-          alignment: Alignment.topLeft,
-          padding: const EdgeInsets.all(10),
-          child: const Text(
-            'Manage Vendors',
-            style: TextStyle(
-              fontWeight: FontWeight.w700,
-              fontSize: 36,
+      child: Column(
+        children: [
+          Container(
+            alignment: Alignment.topLeft,
+            padding: const EdgeInsets.all(10),
+            child: const Text(
+              'Manage Vendors',
+              style: TextStyle(
+                fontWeight: FontWeight.w700,
+                fontSize: 36,
+              ),
             ),
           ),
-        ),
-      );
+          Row(
+            children: [
+              _rowHeader('LOGO', 1),
+              _rowHeader('BUSINESS NAME', 3),
+              _rowHeader('CITY', 2),
+              _rowHeader('STATES', 2),
+              _rowHeader('ACTION', 1),
+              _rowHeader('VIEW MORE', 1),
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
